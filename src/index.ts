6116861a169a4cc98699762fc10a00924cfb9a45
matheuskarnopp <matheus.santos20@estudante.ifms.edu.br>
2026-02-24 09:28:46 -0300

    final da aula 2

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express"
+const app = express()
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("Olá mundo")
+})
+app.listen(8000, ()=>{
+    console.log("Meu servidor está rodando na porta 8000")
+})
