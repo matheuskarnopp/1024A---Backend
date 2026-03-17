6ce13da64f46375ce78445622292e669639d9d6b
matheuskarnopp <matheus.santos20@estudante.ifms.edu.br>
2026-03-17 09:18:22 -0300

    aaaaa

--- src/Aulas Passadas.ts
+++ src/Aulas Passadas.ts
@@ -97,3 +97,36 @@
 // } catch (erro) {
 //     console.log(erro)
 // }
+
+
+
+
+
+
+// //REVISANDO FUNÇÕES USADAS COM ARRAYS
+
+
+// // const vetor:number[] = [1,2,3,4]
+
+// //FIND
+
+// //COMO EU FAÇO UMA FUNÇÃO QUE VERIFICA SE EU
+// //PASSEI O 2 COMO ARGUMENTO PARA ELA
+// //SE SIM RETORNA "TRUE" SENÃO "FALSE"
+
+// // console.log(vetor.find((x)=>x===2))
+
+// const vetor:{id:number, nome:string, idade:number}[] = [
+//     {id:1, nome:"tere", idade:32},
+//     {id:2, nome:"terezinho", idade:12},
+//     {id:3, nome:"terezao", idade:67}
+// ]
+// //FIND achar alguem maior de 18 anos
+// const resultado = vetor.find(((x)=> x.idade>=18))
+// console.log(resultado)
+// //FILTER achar alguem maior de 18
+// const resultado2 = vetor.filter(((x)=> x.idade>=18))
+// console.log(resultado2)
+// //MAP MAPEIA
+// const resultado3 = vetor.filter(((x)=> x.idade))
+// console.log(resultado3)
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,24 @@
-//REVISANDO FUNÇÕES USADAS COM ARRAYS
+// Get the client
+import mysql from 'mysql2/promise';
 
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'luademel',
+});
 
-// const vetor:number[] = [1,2,3,4]
+// Using placeholders
+try {
+  const queryPrepare = await connection.prepare(
+    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+    ['Page', 45]
+  );
 
-//FIND
+  console.log(results);
+} catch (err) {
+  console.log(err);
+}
 
-//COMO EU FAÇO UMA FUNÇÃO QUE VERIFICA SE EU
-//PASSEI O 2 COMO ARGUMENTO PARA ELA
-//SE SIM RETORNA "TRUE" SENÃO "FALSE"
-
-// console.log(vetor.find((x)=>x===2))
-
-const vetor:{id:number, nome:string, idade:number}[] = [
-    {id:1, nome:"tere", idade:32},
-    {id:2, nome:"terezinho", idade:12},
-    {id:3, nome:"terezao", idade:67}
-]
-//FIND achar alguem maior de 18 anos
-const resultado = vetor.find(((x)=> x.idade>=18))
-console.log(resultado)
-//FILTER achar alguem maior de 18
-const resultado2 = vetor.filter(((x)=> x.idade>=18))
-console.log(resultado2)
-//MAP MAPEIA
-const resultado3 = vetor.filter(((x)=> x.idade))
-console.log(resultado3)
+// Close the connection
+await connection.end();
