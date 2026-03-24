46647a58de9bf4498bad6ee030e71d8446c358c7
matheuskarnopp <matheus.santos20@estudante.ifms.edu.br>
2026-03-24 09:26:04 -0300

    alulululululululululullulululululululu8

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 // Get the client
-import mysql, { type RowDataPacket } from 'mysql2/promise';
-interface IPessoa extends RowDataPacket{
-  id:number,
-  nome:string
-}
-// Create the connection to database
-const connection = await mysql.createConnection({
+import mysql, { type RowDataPacket, type Connection } from 'mysql2/promise';
+
+import express from 'express';
+const app = express()
+
+//Como cria uma rota no express?
+app.get("/pessoas",async (req,res)=>{
+
+  let connection:Connection | null = null
+try {
+  const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'luademel',
 });
+  //Como ler as pessoas no bd
+  // Create the connection to database
+
 
 // Using placeholders
-try {
+
   //const result = 
   //  await connection.execute('INSERT INTO pessoa (id,nome) VALUES (?,?)',[6,"Maria"])
   //console.log(result)
@@ -23,15 +30,29 @@ try {
     const element = dados[i];
     console.log(element?.id,element?.nome)
   }
-  console.log(dados);
+  res.status(200).json(dados)
     //console.log(result)
 
 
 } catch (err) {
-  // if(err instanceof Error){
-  //   if(err.)
-  // }
+  if(err instanceof Error){
+    if(connection)
+  
+  await (connection as Connection).end();
+}
   console.log(err);
 }
 // Close the connection
-await connection.end();
+ 
+})
+app.post("pessoas",(req,res)=>{})
+app.listen(8000,()=>{
+  console.log("Iniciando o servidor na porta 8000")
+})
+
+
+
+interface IPessoa extends RowDataPacket{
+  id:number,
+  nome:string
+}
